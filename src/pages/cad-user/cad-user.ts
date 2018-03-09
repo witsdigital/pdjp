f6a4fb10d80b94364d2e58e703dc4aa351f3d87e
witsdigital <34650770+witsdigital@users.noreply.github.com>
2018-03-09 17:20:08 -0300

    update 09/03

--- src/pages/cad-user/cad-user.ts
+++ src/pages/cad-user/cad-user.ts
@@ -23,12 +23,6 @@ export class CadUserPage {
 
   constructor( public navCtrl: NavController, public navParams: NavParams,public toastCtrl:ToastController,public viewCtrl: ViewController, public service: ServiceProvider) {
 
-    this.service.getCidades().subscribe((data)=>{
-      this.cidades = data;
-      console.log(  this.cidades[0]);
-    }, (error)=>{
-      console.log(error);
-    });
 
   }
 
@@ -40,17 +34,26 @@ export class CadUserPage {
   }
   salvar(cad:any){
 
-      this.dadospush = JSON.parse(localStorage.getItem('pushkey'));
+    cad.data = new Date().toISOString();
 
-    cad.userid = this.dadospush.userId;
-        console.log( cad);
-    this.service.postCadUser(cad,'2').then((result)=>{
+    this.service.preCadastroApp(cad).then((result)=>{
       console.log(result);
-this.mensage = result;
-if(this.mensage.mensage==1){
+      this.mensage = result;
+      if(this.mensage.mensage==2){
+
+        let toast = this.toastCtrl.create({
+          message: 'Desculpe, ocorreu um erro. Tente novamente mais tarde',
+          duration: 3000,
+          position: 'top',
+       cssClass: "toast-error"
+        });
+        toast.present();
+      
+      }
+    if(this.mensage.mensage==1){
 
   let toast = this.toastCtrl.create({
-    message: 'Login já esta em uso',
+    message: 'Esse email já esta cadastrado em nosso sistema',
     duration: 3000,
     position: 'top',
  cssClass: "toast-error"
@@ -58,22 +61,11 @@ if(this.mensage.mensage==1){
   toast.present();
 
 }
-if(this.mensage.mensage==2){
+if(this.mensage.mensage==0){
 
   let toast = this.toastCtrl.create({
-    message: 'CPF já esta em uso',
-    duration: 3000,
-    position: 'top',
- cssClass: "toast-error"
-  });
-  toast.present();
-
-}
-if(this.mensage.mensage==3){
-
-  let toast = this.toastCtrl.create({
-    message: 'Cadastro Realizado com sucesso',
-    duration: 3000,
+    message: 'Pré Cadastro realizado com sucesso, em breve entraremos em contato',
+    duration: 6000,
     position: 'top',
  cssClass: "toast-success"
   });
